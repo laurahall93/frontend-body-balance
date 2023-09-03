0bbe25b63f8e61c4e53cbd3c2bd82b9172d5740e
Laura Hall <podinjshmi1993@gmail.com>
2023-09-03 17:17:54 +0100

    add backend url and firebase hosting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtDztkLUrzoYYBt4-xwmpVxfWcvKrNwhg",
+  authDomain: "body-balance-e2.firebaseapp.com",
+  projectId: "body-balance-e2",
+  storageBucket: "body-balance-e2.appspot.com",
+  messagingSenderId: "585060853507",
+  appId: "1:585060853507:web:01282ea8d62f7e90766b36",
+  measurementId: "G-F5SBYKJ4VN",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: "popup",
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
+  callbacks: {
+    // Avoid redirects after sign-in.
+    signInSuccessWithAuthResult: () => false,
+  },
+};
+
+async function sendMessage(message) {
+  const endpoint = "https://bodyandbalance.onrender.com/chat"; // Replace with the appropriate endpoint
+
+  const data = {
+    message: message,
+  };
+
+  const response = await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  const json = await response.json();
+  const reply = json.content;
+  return reply;
+}
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [chats, setChats] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
+  const [isSignedIn, setIsSignedIn] = useState(false); // Local signed-in state.
+
+  const chat = async (e, message) => {
+    e.preventDefault();
+
+    if (!message) return;
+    setIsTyping(true);
+    window.scrollTo(0, 1e10);
+
+    let msgs = chats;
+    msgs.push({ role: "user", content: message });
+    setChats(msgs);
+
+    setMessage("");
+
+    sendMessage(message)
+      .then((response) => {
+        msgs.push({ role: "AI trainer", content: response });
+        setChats(msgs);
+        setIsTyping(false);
+        window.scrollTo(0, 1e10);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // Listen to the Firebase Auth state and set the local state.
+  useEffect(() => {
+    const unregisterAuthObserver = firebase
+      .auth()
+      .onAuthStateChanged((user) => {
+        setIsSignedIn(!!user);
+      });
+    return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
+  }, []);
+
+  if (!isSignedIn) {
+    return (
+      <div>
+        <h1>Body & Balance</h1>
+        <StyledFirebaseAuth
+          uiConfig={uiConfig}
+          firebaseAuth={firebase.auth()}
+        />
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <main>
+      <h1>Chat with Your trainer </h1>
+      <button onClick={() => firebase.auth().signOut()}>Sign-out</button>
+
+      <section>
+        {chats && chats.length
+          ? chats.map((chat, index) => (
+              <p key={index} className={chat.role === "user" ? "user_msg" : ""}>
+                <span>
+                  <b>
+                    {chat.role === "user"
+                      ? firebase.auth().currentUser.displayName
+                      : chat.role.toUpperCase()}
+                  </b>
+                </span>
+                <span>:</span>
+                <span>{chat.content}</span>
+              </p>
+            ))
+          : ""}
+      </section>
+
+      <div className={isTyping ? "" : "hide"}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <i>{isTyping ? "Typing" : ""}</i>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+
+      <form action="" onSubmit={(e) => chat(e, message)}>
+        <input
+          type="text"
+          name="message"
+          value={message}
+          placeholder="Type a message here and hit Enter..."
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
+    </main>
   );
 }
 
